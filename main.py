42b29e9b6222c17dc9aebea85ba3c03f77fc92a1
Vlado Situm <61758568+vsitum@users.noreply.github.com>
2020-12-01 10:33:25 -0800

    ping seccessfully

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
 
+import os
+
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Vlado Situm")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    IpAddress = '13.59.54.144'
+    responce = os.system('ping -n 4 ' + IpAddress)
+
+
     # TODO - Call Ping method and print the results
+    if responce == 0:
+        print()
+        print(IpAddress, 'is up!')
+    else:
+        print(IpAddress, 'is down!')
